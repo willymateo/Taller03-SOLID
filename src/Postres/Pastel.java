1dab3db3659f17eea037b2af83f9c4d304177cf9
Aaron Franco <aaalfran@espol.edu.ec>
2020-11-11 18:23:57 -0500

    Se creo la clase manejador de precio para calcular y mostrar precios de los postres

--- src/Postres/Pastel.java
+++ src/Postres/Pastel.java
@@ -5,9 +5,6 @@
  */
 package Postres;
 
-import java.util.ArrayList;
-import Adicionales.Aderezo;
-
 /**
  *
  * @author Pedro Mendoza
@@ -18,19 +15,9 @@ public class Pastel extends Postre{
         super(sabor);
         this.precioParcial = 15.55;
     }
-    
-    public double calcularPrecioFinal(){
-        double precioFinal;
-        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
-        return precioFinal;
-    }
-
     @Override
     public String toString() {
         return "Pastel{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
     }
-    public String showPrecioFinal(){
-        return "Precio Final: $ " + calcularPrecioFinal();
-    }
-    
+     
 }
---
+++ src/Procesos/ManejadorDePrecio.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Procesos;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class ManejadorDePrecio {
+
+}
